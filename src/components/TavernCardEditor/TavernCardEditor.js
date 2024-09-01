2444d6317fe485c91caf2c32c9f597eb33b3d518
zer0thgear <deepsheepsleep@gmail.com>
2024-08-31 23:36:13 -0500

    Add rudimentary card editing and saving capabilities

--- src/components/TavernCardEditor/TavernCardEditor.js
+++ src/components/TavernCardEditor/TavernCardEditor.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 import {
-    //Button,
+    Button,
     Box,
     Container,
     Paper,
-    //TextField
+    TextField,
     Switch,
     //Typography
 } from '@mui/material'
@@ -13,9 +13,11 @@ import { useTheme } from '@mui/material/styles'
 
 import default_avatar from '../../assets/default_avatar.png';
 import FileUpload from '../FileUpload/FileUpload';
-import { parsePngChunks } from '../../utils/parsePngChunks';
-import { v2CardPrototype, v2CharacterBookEntryPrototype, v2CharacterBookPrototype } from '../../utils/v2CardPrototype';
-import { v3AssetPrototype, v3CardPrototype, v3CharacterBookEntryPrototype, v3CharacterBookPrototype } from '../../utils/v3CardPrototype';
+import assembleNewPng from '../../utils/assembleNewPng';
+import parsePngChunks from '../../utils/parsePngChunks';
+import stripPngChunks from '../../utils/stripPngChunks';
+import { v2CardPrototype, /*v2CharacterBookEntryPrototype, v2CharacterBookPrototype*/ } from '../../utils/v2CardPrototype';
+import { /*v3AssetPrototype,*/ v3CardPrototype, /*v3CharacterBookEntryPrototype, v3CharacterBookPrototype*/ } from '../../utils/v3CardPrototype';
 import './TavernCardEditor.css';
 
 const TavernCardEditor = ({toggleTheme}) => {
@@ -25,6 +27,7 @@ const TavernCardEditor = ({toggleTheme}) => {
     const [cardDataV3, setCardDataV3] = useState(v3CardPrototype);
     const [file, setFile] = useState();
     const [preview, setPreview] = useState(default_avatar);
+    const [useV3Spec, setUseV3Spec] = useState(false);
 
     async function handleFileSelect(event) {
         const selectedFile = event.target.files[0];
@@ -43,6 +46,7 @@ const TavernCardEditor = ({toggleTheme}) => {
                             setCardDataV3(parsedCardData[item].data);
                             console.log("V3 Card info found");
                             console.log(parsedCardData[item].data);
+                            setUseV3Spec(true);
                         } else if (parsedCardData[item].keyword === "chara"){
                             console.log("V2 card info found");
                             setCardDataV2(parsedCardData[item].data);
@@ -51,7 +55,7 @@ const TavernCardEditor = ({toggleTheme}) => {
                     }
                 }
                 else {
-                    console.log("Only V2 card info found")
+                    console.log("Only V2 card info found");
                     setCardDataV2(parsedCardData[0].data);
                     console.log(parsedCardData[0].data);
                 }
@@ -64,16 +68,80 @@ const TavernCardEditor = ({toggleTheme}) => {
             //console.log(parsedJson.spec);
             //console.log(parsedJson.spec_version);
             console.log(parsedJson);
+            console.log(parsedJson.spec === "chara_card_v3");
+            setUseV3Spec(parsedJson.spec === "chara_card_v3");
             setCardDataV2(parsedJson);
         }
         //console.log(selectedFile.name);
         //console.log(pngRegex.test(selectedFile.name));
     }
 
+    function handleJsonDownload() {
+        const blob = new Blob([JSON.stringify(cardDataV2, null, 4)], { type: 'application/json' });
+
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `${cardDataV2.data.name}.json`
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+
+        URL.revokeObjectURL(url);
+    }
+
+    async function handlePngDownload() {
+        try {
+            const response = await fetch(preview);
+            if (!response.ok){
+                throw new Error("Network response was not OK");
+            }
+            const respBlob = await response.blob();
+            const arrayBuffer = await respBlob.arrayBuffer();
+            const strippedPng = await stripPngChunks(arrayBuffer);
+            const assembledPng = await assembleNewPng(strippedPng, useV3Spec ? [{keyword:"chara", data:cardDataV2}, {keyword:"ccv3", data:cardDataV3}] : {keyword:"chara", data:cardDataV2});
+            console.log("Saving as V3: ", useV3Spec);
+            const blob = new Blob([assembledPng], { type: 'image/png' });
+
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${cardDataV2.data.name}.png`
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+
+            URL.revokeObjectURL(url);
+        } catch (error) {
+            console.error("There was an error retrieving the card's PNG: ", error)
+        }
+        
+    }
+
     function handleRemoveFile() {
         setFile(null);
         setPreview(default_avatar);
+        setCardDataV2(v2CardPrototype);
+        setCardDataV3(v3CardPrototype);
     }
+
+    const handleTextFieldChange = (e) => {
+        const {name, value} = e.target;
+        setCardDataV2((prevState) => ({
+            ...prevState,
+            data: {
+                ...prevState.data,
+                [name]: value,
+            }
+        }));
+        setCardDataV3((prevState) => ({
+            ...prevState,
+            data: {
+                ...prevState.data,
+                [name]: value,
+            }
+        }));
+    };
 
     useEffect(() => {
         const pngRegex = /.+\.png$/;
@@ -98,8 +166,18 @@ const TavernCardEditor = ({toggleTheme}) => {
                 <Box style={{display:'flex', alignItems:'center'}}>Light <Switch checked={theme.palette.mode === "dark"} onChange={toggleTheme}/> Dark</Box>
             </Container>   
             <Paper elevation={6}>
-                <Container disableGutters maxWidth={false}>
-                    <img alt={file ? file.name : "No avatar loaded"} src={preview}/>
+                <Container disableGutters maxWidth={false} style={{display:"flex", height:"90vh"}}>
+                    <Container disableGutters style={{alignItems:"center", display:"flex", flex:2, overflow:"auto"}} sx={{ml:2}}>
+                        <img alt={file ? file.name : "No avatar loaded"} src={preview}/>
+                    </Container>
+                    <Container disableGutters maxWidth={false} style={{display:"flex", flexDirection:"column", flex:5, margin:10, overflow:"auto"}}>
+                        <TextField fullWidth label="Card name" margin="normal" name="name" onChange={handleTextFieldChange} value={cardDataV3.data.name !== "" ? cardDataV3.data.name : cardDataV2.data.name}/>
+                        <TextField fullWidth label="Card desciption" margin="normal" name="description" multiline rows={10}  onChange={handleTextFieldChange} value={cardDataV3.data.description !== "" ? cardDataV3.data.description : cardDataV2.data.description}/>
+                        <Container disableGutters maxWidth={false} style={{display:"flex", justifyContent:'space-between'}}>
+                            <Button onClick={handleJsonDownload} variant="contained">Download as JSON</Button>
+                            <Button onClick={handlePngDownload} variant="contained">Download as PNG</Button>
+                        </Container>
+                    </Container>
                 </Container>
             </Paper>
         </Container>
