0089714236b8b0e1c170f74cc65929cca168ad61
zer0thgear <deepsheepsleep@gmail.com>
2024-09-02 23:36:08 -0500

    Change basic callback so that it works properly with tags

--- src/components/TabPanels/TabPanels.js
+++ src/components/TabPanels/TabPanels.js
@@ -23,17 +23,35 @@ export function BasicFieldTabPanel ({curTab, index, arrayToIterate, useV3Spec, c
             ...prevState,
             data: {
                 ...prevState.data,
-                [name]: value,
+                [name]: value
             }
         }));
         setCardDataV3((prevState) => ({
             ...prevState,
             data: {
                 ...prevState.data,
-                [name]: value,
+                [name]: value
             }
         }));
     };
+
+    const handleTagChange = (e) => {
+        const {name, value} = e.target;
+        setCardDataV2((prevState) => ({
+            ...prevState,
+            data: {
+                ...prevState.data,
+                [name]: value.split(",")
+            }
+        }));
+        setCardDataV3((prevState) => ({
+            ...prevState,
+            data: {
+                ...prevState.data,
+                [name]: value.split(",")
+            }
+        }));
+    }
 
     return(
         <div hidden={curTab !== index}>
@@ -44,7 +62,7 @@ export function BasicFieldTabPanel ({curTab, index, arrayToIterate, useV3Spec, c
                 label={Object.hasOwn(field, "label") ? field.label : ""} 
                 multiline={Object.hasOwn(field, "multiline") ? field.multiline : false} 
                 rows={Object.hasOwn(field, "rows") ? field.rows : 1}
-                changeCallback={handleTextFieldChange} useV3Spec={useV3Spec} cardDataV2={cardDataV2} cardDataV3={cardDataV3}
+                changeCallback={field.fieldName === "tags" ? handleTagChange : handleTextFieldChange} useV3Spec={useV3Spec} cardDataV2={cardDataV2} cardDataV3={cardDataV3}
             />
             ))}
         </div>
